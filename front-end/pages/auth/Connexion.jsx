f2250ee959809784bcee01b7870052ebad4ab825
RichardNk24 <richardsobelnkulu@gmail.com>
2022-12-08 10:00:03 +0200

    New Sign In & Sign Up

---
+++ front-end/pages/auth/Connexion.jsx
@@ -0,0 +1,60 @@
+    import React from "react";
+    import * as Components from './Components';
+    import  AutoGreet from '../../components/AutoGreet'
+
+    function Connexion() {
+        const [signIn, toggle] = React.useState(true);
+        return(
+            <Components.Container>
+                <Components.SignUpContainer signinIn={signIn}>
+                    <Components.Form>
+                        <Components.Title>Créer un Compte</Components.Title>
+                        <Components.Input type='text' placeholder='Nom Complet' />
+                        <Components.Input type='email' placeholder='E-mail' />
+                        <Components.Input type='password' placeholder='Mot de Passe' />
+                        <Components.Input type='password' placeholder='Confirmez le mot de passe' />
+                        <Components.Button>Je m'inscris</Components.Button>
+                    </Components.Form>
+                </Components.SignUpContainer>
+
+                <Components.SignInContainer signinIn={signIn}>
+                    <Components.Form>
+                        <Components.Title>Connexion</Components.Title>
+                        <Components.Input type='email' placeholder='E-mail' />
+                        <Components.Input type='password' placeholder='Mot de Passe' />
+                        <Components.Anchor href='#'>J'ai oublié le mot de passe !</Components.Anchor>
+                        <Components.Button>Connexion</Components.Button>
+                    </Components.Form>
+                </Components.SignInContainer>
+
+                <Components.OverlayContainer signinIn={signIn}>
+                    <Components.Overlay signinIn={signIn}>
+
+                    <Components.LeftOverlayPanel signinIn={signIn}>
+                        <Components.Title>Bon retour!</Components.Title>
+                        <Components.Paragraph>
+                            Pour utiliser la plate-forme, veuillez vous connecter à l'aide vos identifiants.
+                        </Components.Paragraph>
+                        <Components.GhostButton onClick={() => toggle(true)}>
+                            Connexion
+                        </Components.GhostButton>
+                        </Components.LeftOverlayPanel>
+
+                        <Components.RightOverlayPanel signinIn={signIn}>
+                            <Components.Title><AutoGreet /></Components.Title>
+                            <Components.Paragraph>
+                                    Inscrivez-vous et commencez votre expérience avec nous
+                            </Components.Paragraph>
+                                <Components.GhostButton onClick={() => toggle(false)}>
+                                    Inscription
+                                </Components.GhostButton> 
+                        </Components.RightOverlayPanel>
+    
+                    </Components.Overlay>
+                </Components.OverlayContainer>
+
+            </Components.Container>
+        )
+    }
+
+    export default Connexion;
--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -2,21 +2,19 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {AccountBox} from './auth/accountBox';
-
+import Connexion from './auth/Connexion'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Accueil</title>
+        <title>Connexion</title>
         <meta name="description" content="Application created by Richard Nkulu" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        
-        <AccountBox />
+        <Connexion />
       </main>
 
       <footer className={styles.footer}>
