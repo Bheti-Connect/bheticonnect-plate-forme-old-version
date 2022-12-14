aafad72e8d77554ea94a21a61e2016420b81fa55
Banitongwa David <davidbanitongwa@gmail.com>
2022-12-14 09:05:16 +0100

    allcards and add themes

---
+++ front-end/components/cards/AllCards.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AllCards = () => {
+
+
+
+    
+  return (
+    <div>AllCards</div>
+  )
+}
+
+export default AllCards
--- front-end/components/cards/Cards.jsx
+++ front-end/components/cards/Cards.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components';
 
 const Cards = ({item, setSelect, setModal}) => {
 
