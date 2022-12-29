cf055bdb633477be2be4717b1d90f240a769701d
Banitongwa David <davidbanitongwa@gmail.com>
2022-12-29 20:53:14 +0100

    add NavMenu

--- front-end/pages/Administrateur/Accueil.jsx
+++ front-end/pages/Administrateur/Accueil.jsx
@@ -1,10 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import NavMenu from './NavMenu'
+import HeaderAdmin from './HeaderAdmin'
+import Example from './charts/Example'
+
 
 const Accueil = () => {
+
+
+
     return (
-        <div>
-            <h1>Administrateur</h1>
-        </div>
+    <Container>
+        <NavMenu/>
+        <HeaderAdmin />
+        <Example />
+
+    </Container>
     )
 }
+
+// style CSS
+const Container = styled.div`
+
+`;
 
 export default Accueil;
